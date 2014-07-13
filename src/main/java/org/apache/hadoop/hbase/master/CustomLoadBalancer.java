ddea30b51e7191a2d55683e04eaa5ad3c36e1c06
Gomathivinayagam Muthuvinayagam <gomes@yahoo-inc.com>
2014-07-12 19:13:54 -0700

    Adding force custom load balancer

--- src/main/java/org/apache/hadoop/hbase/master/CustomLoadBalancer.java
+++ src/main/java/org/apache/hadoop/hbase/master/CustomLoadBalancer.java
@@ -3,8 +3,6 @@ package org.apache.hadoop.hbase.master;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,6 +18,8 @@ import org.apache.hadoop.hbase.ClusterStatus;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.ServerName;
 
+import com.google.common.collect.ArrayListMultimap;
+
 /**
  * 
  * A force load balancer which makes sure related regions are placed on the same
@@ -29,13 +29,44 @@ import org.apache.hadoop.hbase.ServerName;
  * 
  * TODO: This implementation assumes all tables are related to each other. Tries
  * to place regions of the tables on the same region server. If an unrelated
- * table will be managed by the Force server, then we might need to revisit this
+ * table will be managed by the Force server, then we may need to revisit this
  * class to change the placement logic for those unrelated regions.
  */
 
 public class CustomLoadBalancer extends DefaultLoadBalancer {
 	private static final Log LOG = LogFactory.getLog(LoadBalancer.class);
 	private static final Random RANDOM = new Random(System.currentTimeMillis());
+
+	private static class RegionStartEndKeyId {
+		private final byte[] startKey, endKey;
+
+		public RegionStartEndKeyId(byte[] startKey, byte[] endKey) {
+			this.startKey = startKey;
+			this.endKey = endKey;
+		}
+
+		@Override
+		public int hashCode() {
+			int prime = 31;
+			int result = Arrays.hashCode(startKey);
+			result = result * prime + Arrays.hashCode(endKey);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object anoObj) {
+			if (anoObj == null || !(anoObj instanceof RegionStartEndKeyId))
+				return false;
+			if (this == anoObj)
+				return true;
+			RegionStartEndKeyId regionStartEndKeyObj = (RegionStartEndKeyId) anoObj;
+			if (Arrays.equals(startKey, regionStartEndKeyObj.startKey)
+					&& Arrays.equals(endKey, regionStartEndKeyObj.endKey))
+				return true;
+			return false;
+		}
+	}
+
 	private static final ClusterDataKeyGenerator<HRegionInfo, RegionStartEndKeyId> REGION_KEY_GEN = new ClusterDataKeyGenerator<HRegionInfo, RegionStartEndKeyId>() {
 
 		@Override
@@ -104,39 +135,108 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 			Map<HRegionInfo, ServerName> regions, List<ServerName> servers) {
 		// TODO Auto-generated method stub
 		Map<ServerName, List<HRegionInfo>> result = new TreeMap<ServerName, List<HRegionInfo>>();
-		for (ServerName server : servers)
-			result.put(server, new ArrayList<HRegionInfo>());
 
-		Set<ServerName> unavailableServers = minus(servers, regions.values());
-		Collection<List<HRegionInfo>> clusteredRegions = clusterRegions(regions
+		List<ServerName> allUnavailServers = minus(regions.values(), servers);
+		Map<String, List<ServerName>> allAvailClusteredServers = clusterServers(servers);
+		Collection<List<HRegionInfo>> clusteredRegionGroups = clusterRegions(regions
 				.keySet());
 
-		for (List<HRegionInfo> cluster : clusteredRegions) {
-			Map<HRegionInfo, ServerName> regionInfoAndServerForCluster = getEntriesFrom(
-					regions, cluster);
-			List<ServerName> clusterServers = new ArrayList<ServerName>(
-					removeDuplicates(regionInfoAndServerForCluster.values()));
-			Set<ServerName> unavailableServersForCluster = intersect(
-					unavailableServers, clusterServers);
-			Collections.sort(clusterServers, new UnavailableServerComparator(
-					unavailableServersForCluster));
+		for (List<HRegionInfo> clusteredRegionGroup : clusteredRegionGroups) {
+			Map<HRegionInfo, ServerName> regionAndServerNameMap = getMapEntriesForKeys(
+					regions, clusteredRegionGroup);
 
-			int indFirstAvailServer = 0;
-			int indLastAvailServer = clusterServers.size()
-					- unavailableServersForCluster.size() - 1;
+			Map<ServerName, List<HRegionInfo>> partialResult = retainAssignmentCluster(
+					regionAndServerNameMap, allAvailClusteredServers,
+					allUnavailServers);
 
-			if (clusterServers
-					.get(indFirstAvailServer)
-					.getHostname()
-					.compareTo(
-							clusterServers.get(indLastAvailServer)
-									.getHostname()) == 0) {
-
-			} else {
-
+			for (Map.Entry<ServerName, List<HRegionInfo>> partialEntry : partialResult
+					.entrySet()) {
+				if (!result.containsKey(partialEntry.getKey()))
+					result.put(partialEntry.getKey(),
+							new ArrayList<HRegionInfo>());
+				result.get(partialEntry.getKey()).addAll(
+						partialEntry.getValue());
 			}
+
 		}
 		return result;
+	}
+
+	private Map<ServerName, List<HRegionInfo>> retainAssignmentCluster(
+			Map<HRegionInfo, ServerName> regions,
+			Map<String, List<ServerName>> allAvailClusteredServers,
+			List<ServerName> allUnavailServers) {
+		Map<ServerName, List<HRegionInfo>> result = new TreeMap<ServerName, List<HRegionInfo>>();
+		ArrayListMultimap<ServerName, HRegionInfo> serverNameAndRegionsMap = reverseMap(regions);
+
+		List<ServerName> servers = new ArrayList<ServerName>(
+				removeDuplicates(regions.values()));
+		List<ServerName> unavailServers = intersect(allUnavailServers, servers);
+		List<ServerName> availServers = minus(servers, unavailServers);
+		List<HRegionInfo> unplacedRegions = findUnplacedRegions(
+				serverNameAndRegionsMap, unavailServers);
+
+		for (ServerName unavailServer : unavailServers)
+			serverNameAndRegionsMap.removeAll(unavailServer);
+
+		List<List<ServerName>> bestPlacementRegionClusters;
+		if (availServers.size() == 0)
+			bestPlacementRegionClusters = new ArrayList<List<ServerName>>(
+					allAvailClusteredServers.values());
+		else {
+			ServerName serverWithMajorityRegions = findServerNameWithMajorityRegions(serverNameAndRegionsMap);
+			bestPlacementRegionClusters = new ArrayList<List<ServerName>>();
+			bestPlacementRegionClusters.add(allAvailClusteredServers
+					.get(serverWithMajorityRegions.getHostname()));
+
+			for (ServerName server : serverNameAndRegionsMap.keySet()) {
+				if (!server.getHostname().equals(
+						serverWithMajorityRegions.getHostname()))
+					unplacedRegions.addAll(serverNameAndRegionsMap.get(server));
+			}
+		}
+
+		Map<HRegionInfo, ServerName> assignment = immediateAssignmentCluster(
+				unplacedRegions, bestPlacementRegionClusters);
+		for (Map.Entry<HRegionInfo, ServerName> entry : assignment.entrySet()) {
+			if (!result.containsKey(entry.getValue()))
+				result.put(entry.getValue(), new ArrayList<HRegionInfo>());
+			result.get(entry.getValue()).add(entry.getKey());
+		}
+		return result;
+	}
+
+	private ServerName findServerNameWithMajorityRegions(
+			ArrayListMultimap<ServerName, HRegionInfo> serverNameAndRegionsMap) {
+		ServerName result = null;
+		int maxRegionsSize = -1;
+		for (ServerName serverName : serverNameAndRegionsMap.keySet()) {
+			if (maxRegionsSize < serverNameAndRegionsMap.get(serverName).size()) {
+				maxRegionsSize = serverNameAndRegionsMap.size();
+				result = serverName;
+			}
+		}
+
+		return result;
+	}
+
+	private List<HRegionInfo> findUnplacedRegions(
+			ArrayListMultimap<ServerName, HRegionInfo> regionsAndServers,
+			List<ServerName> unavailableServers) {
+		List<HRegionInfo> hRegions = new ArrayList<HRegionInfo>();
+		for (ServerName serverName : unavailableServers)
+			if (regionsAndServers.containsKey(serverName))
+				hRegions.addAll(regionsAndServers.get(serverName));
+		return hRegions;
+	}
+
+	private static <K, V> ArrayListMultimap<V, K> reverseMap(Map<K, V> input) {
+		ArrayListMultimap<V, K> multiMap = ArrayListMultimap.create();
+		for (Map.Entry<K, V> entry : input.entrySet()) {
+			multiMap.put(entry.getValue(), entry.getKey());
+		}
+
+		return multiMap;
 	}
 
 	private static <A> Collection<A> removeDuplicates(Collection<A> input) {
@@ -148,85 +248,6 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 	}
 
 	/**
-	 * Used for sorting list of {@link ServerName}, with additional parameter
-	 * unavailable servers. Given two servers fServer and sServer, if just one
-	 * of them in the unavailable servers list, and it is returned to be greater
-	 * element among the two.
-	 * 
-	 * Usage: In a given collection of servers and sorting the collection using
-	 * this comparator, will make the unavailable servers always bubbled to the
-	 * end of the list during the sorting.
-	 * 
-	 */
-	private static class UnavailableServerComparator implements
-			Comparator<ServerName> {
-
-		private final Set<ServerName> missingServers;
-
-		public UnavailableServerComparator(Set<ServerName> missingServers) {
-			this.missingServers = missingServers;
-		}
-
-		@Override
-		public int compare(ServerName fServer, ServerName sServer) {
-			// TODO Auto-generated method stub
-			int fExists = missingServers.contains(fServer) ? 1 : 0;
-			int sExists = missingServers.contains(sServer) ? 1 : 0;
-			if (fExists == sExists)
-				return fServer.compareTo(sServer);
-			return fExists - sExists;
-		}
-
-	}
-
-	private static <K, V> Map<K, V> getEntriesFrom(Map<K, V> map,
-			Collection<K> keys) {
-		Map<K, V> result = new HashMap<K, V>();
-		for (K key : keys)
-			if (map.containsKey(key))
-				result.put(key, map.get(key));
-		return result;
-	}
-
-	/**
-	 * Returns the common elements of a and b
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
-	private static Set<ServerName> intersect(Collection<ServerName> a,
-			Collection<ServerName> b) {
-		Set<ServerName> hashedEntriesA = new HashSet<ServerName>();
-		hashedEntriesA.addAll(a);
-
-		Set<ServerName> result = new HashSet<ServerName>();
-		for (ServerName serverName : b)
-			if (hashedEntriesA.contains(serverName))
-				result.add(serverName);
-		return result;
-	}
-
-	/**
-	 * Returns the result of (a - b)
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
-	private static Set<ServerName> minus(Collection<ServerName> a,
-			Collection<ServerName> b) {
-		Set<ServerName> hashedEntriesB = new HashSet<ServerName>();
-		hashedEntriesB.addAll(b);
-
-		Set<ServerName> result = new HashSet<ServerName>();
-		for (ServerName serverName : a)
-			if (!hashedEntriesB.contains(serverName))
-				result.add(serverName);
-		return result;
-	}
-
-	/**
 	 * Immediate assignment of regions to servers. Does not consider the best
 	 * way to assign regions. Makes sure related regions are assigned to the
 	 * same region server.
@@ -235,21 +256,48 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 	public Map<HRegionInfo, ServerName> immediateAssignment(
 			List<HRegionInfo> regions, List<ServerName> servers) {
 		// TODO Auto-generated method stub
-		Collection<List<HRegionInfo>> clusteredRegionGroups = clusterRegions(regions);
-		List<List<ServerName>> clusterdServerGroups = clusterServers(servers);
+		List<List<HRegionInfo>> clusteredRegionGroups = clusterRegions(regions);
+		List<List<ServerName>> clusterdServerGroups = new ArrayList<List<ServerName>>(
+				clusterServers(servers).values());
 		Map<HRegionInfo, ServerName> assignments = new TreeMap<HRegionInfo, ServerName>();
 
 		for (List<HRegionInfo> clusterRegionGroup : clusteredRegionGroups) {
-			List<ServerName> clusteredServerGroup = clusterdServerGroups
-					.get(RANDOM.nextInt(clusterdServerGroups.size()));
-			int i = 0;
-			int size = clusteredServerGroup.size();
-			for (HRegionInfo hRegionInfo : clusterRegionGroup) {
-				assignments.put(hRegionInfo, clusteredServerGroup.get(i));
-				i = (i + 1) % size;
-			}
+			assignments.putAll(immediateAssignmentCluster(clusterRegionGroup,
+					clusterdServerGroups));
 		}
 		return assignments;
+	}
+
+	private Map<HRegionInfo, ServerName> immediateAssignmentCluster(
+			List<HRegionInfo> clusteredRegionGroup,
+			List<List<ServerName>> clusterdServerGroups) {
+		// TODO Auto-generated method stub
+		Map<HRegionInfo, ServerName> assignments = new TreeMap<HRegionInfo, ServerName>();
+
+		List<ServerName> clusteredServerGroup = findBestCluster(clusterdServerGroups);
+		int i = 0;
+		int size = clusteredServerGroup.size();
+		for (HRegionInfo hRegionInfo : clusteredRegionGroup) {
+			assignments.put(hRegionInfo, clusteredServerGroup.get(i));
+			i = (i + 1) % size;
+		}
+
+		return assignments;
+	}
+
+	/**
+	 * Current implementation gives a random cluster group from all the groups.
+	 * 
+	 * TODO: Using history metrics of clustered server groups, we can find the
+	 * best cluster server group.
+	 * 
+	 * @param allClusteredServerGroups
+	 * @return
+	 */
+	private List<ServerName> findBestCluster(
+			List<List<ServerName>> allClusteredServerGroups) {
+		return allClusteredServerGroups.get(RANDOM
+				.nextInt(allClusteredServerGroups.size()));
 	}
 
 	@Override
@@ -262,19 +310,6 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 		K generateKey(V v);
 	}
 
-	private static <K, V> List<List<V>> cluster(Collection<V> input,
-			ClusterDataKeyGenerator<V, K> keyGenerator) {
-		Map<K, List<V>> result = new HashMap<K, List<V>>();
-		for (V v : input) {
-			K k = keyGenerator.generateKey(v);
-			if (!result.containsKey(k))
-				result.put(k, new ArrayList<V>());
-			result.get(k).add(v);
-		}
-
-		return new ArrayList<List<V>>(result.values());
-	}
-
 	/**
 	 * Groups the servers based on the hostname. If multiple region servers are
 	 * running on the same host, then this will group them into a group.
@@ -283,7 +318,7 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 	 * @return
 	 */
 
-	private static List<List<ServerName>> clusterServers(
+	private static Map<String, List<ServerName>> clusterServers(
 			Collection<ServerName> servers) {
 		return cluster(servers, SERVER_KEY_GEN);
 	}
@@ -294,39 +329,71 @@ public class CustomLoadBalancer extends DefaultLoadBalancer {
 	 * @param regions
 	 * @return
 	 */
-	private static Collection<List<HRegionInfo>> clusterRegions(
+	private static List<List<HRegionInfo>> clusterRegions(
 			Collection<HRegionInfo> regions) {
-		return cluster(regions, REGION_KEY_GEN);
+		return new ArrayList<List<HRegionInfo>>(
+				cluster(regions, REGION_KEY_GEN).values());
 	}
 
-	private static class RegionStartEndKeyId {
-		private final byte[] startKey, endKey;
+	// Common functions
 
-		public RegionStartEndKeyId(byte[] startKey, byte[] endKey) {
-			this.startKey = startKey;
-			this.endKey = endKey;
+	private static <K, V> Map<K, List<V>> cluster(Collection<V> input,
+			ClusterDataKeyGenerator<V, K> keyGenerator) {
+		Map<K, List<V>> result = new HashMap<K, List<V>>();
+		for (V v : input) {
+			K k = keyGenerator.generateKey(v);
+			if (!result.containsKey(k))
+				result.put(k, new ArrayList<V>());
+			result.get(k).add(v);
 		}
 
-		@Override
-		public int hashCode() {
-			int prime = 31;
-			int result = Arrays.hashCode(startKey);
-			result = result * prime + Arrays.hashCode(endKey);
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object anoObj) {
-			if (anoObj == null || !(anoObj instanceof RegionStartEndKeyId))
-				return false;
-			if (this == anoObj)
-				return true;
-			RegionStartEndKeyId regionStartEndKeyObj = (RegionStartEndKeyId) anoObj;
-			if (Arrays.equals(startKey, regionStartEndKeyObj.startKey)
-					&& Arrays.equals(endKey, regionStartEndKeyObj.endKey))
-				return true;
-			return false;
-		}
+		return result;
 	}
 
+	private static <K, V> Map<K, V> getMapEntriesForKeys(Map<K, V> map,
+			Collection<K> keys) {
+		Map<K, V> result = new HashMap<K, V>();
+		for (K key : keys)
+			if (map.containsKey(key))
+				result.put(key, map.get(key));
+		return result;
+	}
+
+	/**
+	 * Returns the common elements of a and b. Note: Result is not a mutli bag
+	 * operation, the input collections are converted into set equivalents.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	private static <T> List<T> intersect(Collection<T> a, Collection<T> b) {
+		Set<T> hashedEntriesA = new HashSet<T>();
+		hashedEntriesA.addAll(a);
+
+		Set<T> result = new HashSet<T>();
+		for (T in : b)
+			if (hashedEntriesA.contains(in))
+				result.add(in);
+		return new ArrayList<T>(result);
+	}
+
+	/**
+	 * Returns the result of (a - b). Note: Result is not a mutli bag operation,
+	 * the input collections are converted into set equivalents.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	private static <T> List<T> minus(Collection<T> a, Collection<T> b) {
+		Set<T> hashedEntriesB = new HashSet<T>();
+		hashedEntriesB.addAll(b);
+
+		Set<T> result = new HashSet<T>();
+		for (T serverName : a)
+			if (!hashedEntriesB.contains(serverName))
+				result.add(serverName);
+		return new ArrayList<T>(result);
+	}
 }
